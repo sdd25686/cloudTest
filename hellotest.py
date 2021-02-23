679a329a2b8cb821c96367f840bb60f3cb774c99
sdd25686 <sdd25686@gmail.com>
2021-02-23 10:58:59 +0700

    test

--- hellotest.py
+++ hellotest.py
@@ -1,5 +1,22 @@
-from flask import flaskapp=Flask(__name__)
+from flask import Flask
+app=Flask(__name__)
+
+@app.route('/')
+def Home():
+    return "<h1>Hello World</h1>"
+
+@app.route('/John')
+def John():
+    return "<h1>Hello John</h1>"
+
+@app.route("/")
+def Hello():
+    return 'Open a new tab and enter /Welcome/name for URL'
+
+@app.route('/Welcome/<name>')
+def Welcome_name(name):
+    return 'Welcome ' + name +'!'
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=80)               
